9ac43494aafbe8ede70fcc31492b710edf1fda2d
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-26 15:49:37 +0100

    Añadidos ficheros

---
+++ testingGitHub/src/main/java/tester/TestingGitHubApplication.java
@@ -0,0 +1,13 @@
+package tester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingGitHubApplication.class, args);
+	}
+
+}
---
+++ testingGitHub/src/test/java/tester/TestingGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
