cacea717fdfd6acab56f7703ebf241680ba73665
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-02-06 12:39:58 +0100

    Añadidas bases de datos

---
+++ 17_app_coches_mongoose/bbdd/coches.js
@@ -0,0 +1,53 @@
+const coches = 
+[
+    {"marca": "Renault",
+     "modelo": "Renault Sapace",
+     "precio": 19500,
+     "anio": 2008,
+     "extras": ["aire","llantas aleacion"]
+    },
+    {"marca": "Renault",
+     "modelo": "Renault megane 1.4",
+     "precio": 8000,
+     "anio": 2010,
+     "extras": ["aire","ABS"]
+    },
+    {"marca": "Renault",
+     "modelo": "Renault Clio 1.9 TD",
+     "precio": 7800,
+     "anio": 2010,
+     "extras": ["Lunas tintadas","ESP"]
+    },
+    {"marca": "Seat",
+     "modelo": "Ibiza 1.0 tdi",
+     "precio": 5600,
+     "anio": 2011,
+     "extras": ["aire"]
+    },
+    {"marca": "Seat",
+     "modelo": "Ibiza sport 5",
+     "precio": 11500,
+     "anio": 2012,
+     "extras": ["llantas aleacion","Garantia 1 año"]
+    },
+    {"marca": "Seta",
+     "modelo": "Toledo 1.4 elegance",
+     "precio": 8900,
+     "anio": 2008,
+     "extras": ["Asientos calefactables","Volante deportivo"]
+    },
+    {"marca": "Citroen",
+     "modelo": "Picasso 1.9 TD",
+     "precio": 6700,
+     "anio": 2007,
+     "extras": ["llantas aleacion"]
+    },
+    {"marca": "Citroen",
+     "modelo": "C4 1.8",
+     "precio": 9000,
+     "anio": 2007,
+     "extras": ["Volante deportivo"]
+    }
+    ];
+
+    module.exports = coches
---
+++ 17_app_coches_mongoose/bbdd/usuarios.js
@@ -0,0 +1,41 @@
+const usuarios = 
+[
+    {
+        "username": "tomas",
+        "password": "tomasin",
+        "nombre": "Tomas Escudero",
+        "email": "tomas@fp.com",
+        "rol": "ADMON"
+    },
+    {
+        "username": "eva",
+        "password": "evita",
+        "nombre": "Eva Goma",
+        "email": "eva@fp.com",
+        "rol": "CLIENTE"
+    },
+    {
+        "username": "carlos",
+        "password": "carlitos",
+        "nombre": "Carlos Lopez",
+        "email": "carlos@fp.com",
+        "rol": "CLIENTE"
+    },
+    {
+        "username": "anastasio",
+        "password": "anastasio",
+        "nombre": "Anastasio Aroca",
+        "email": "anastasio@fp.com",
+        "rol": "CLIENTE"
+    },
+
+    {
+        "username": "sara",
+        "password": "sarita",
+        "nombre": "Sara Varas",
+        "email": "sara@fp.com",
+        "rol": "DIRECTOR"
+    }
+]
+
+module.exports = usuarios;
