d2fe15a715518f1ee52d5f10cb42eaf43d057ae1
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-02-09 16:46:42 +0100

    Añadindo detalles para usuarios

--- 17_app_coches_mongoose/index.js
+++ 17_app_coches_mongoose/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 
 const rutaCoches = require('./routes/coche.routes');
+const rutaUsuarios = require('./routes/usuario.routes');
 
 //Middleware
 app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use('/api/coches', rutaCoches);
+app.use('/api/usuarios', rutaUsuarios);
 
 //conexion a mongoose
 mongoose
-	.connect('mongodb://127.0.0.1:27017/ejemplobbdd')
+	.connect('mongodb://127.0.0.1:27017/bbdd-coches_coches_2026_Unir_daw')
 	.then(() => console.log('base de datos de Mongo en marcha...'))
 	.catch(() => console.log('BBDD NO arrancada'));
 //levantar el servicio
