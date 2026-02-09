587b9a8e642cdb210c55b34490b639472dd7c6e8
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-02-09 16:47:22 +0100

    Create usuario.controllers.js

---
+++ 17_app_coches_mongoose/controller/usuario.controllers.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const ruta = express.Router();
+const Usuario = require('../model/usuario.models');
+const usuarios = require('../bbdd/usuarios');
+
+class UsuarioController {
+	async cargaInicial(req, res) {
+		await Usuario.insertMany(usuarios);
+		return res.status(201).send('Carga de usuarios completada.');
+	}
+
+	async findByUserNameAndPassword(req, res) {}
+
+	async registrar(req, res) {}
+
+	async findById(req, res) {}
+}
+
+module.exports = new UsuarioController();
