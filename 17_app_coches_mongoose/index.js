82aca6edfeb6576382ba6991621393bdff7bb2f6
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-02-10 16:13:56 +0100

    Update index.js

--- 17_app_coches_mongoose/index.js
+++ 17_app_coches_mongoose/index.js
@@ -5,12 +5,14 @@ const bodyparser = require('body-parser');
 
 const rutaCoches = require('./routes/coche.routes');
 const rutaUsuarios = require('./routes/usuario.routes');
+const rutaVentas = require('./routes/venta.routes');
 
 //Middleware
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use('/api/coches', rutaCoches);
 app.use('/api/usuarios', rutaUsuarios);
+app.use('/api/ventas', rutaVentas);
 
 //conexion a mongoose
 mongoose
